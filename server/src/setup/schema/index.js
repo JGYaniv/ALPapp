58e8ec6f00dbb8df1ff330bc2df9cf27866f96f8
zohaib <khaliqzohaib92@gmail.com>
2020-08-12 22:08:33 -0400

    Create graphql schema using queries and mutations

---
+++ server/src/setup/schema/index.js
@@ -0,0 +1,11 @@
+const { GraphQLSchema }  =  require("graphql");
+
+const mutation = require("./mutations");
+const query = require("./queries");
+
+const schema = new GraphQLSchema({
+    query,
+    mutation 
+});
+
+export default schema;
