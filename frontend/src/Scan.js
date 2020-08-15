95233fef5c896d32fe15b0aca15a8b9e7608d621
LouisVelz <velazquezlouis2@gmail.com>
2020-08-15 12:23:01 -0400

    added npm camera, npm install in frontend

--- frontend/src/Scan.js
+++ frontend/src/Scan.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import Camera from 'react-native-camera';
 
-export default function Scan({ navigation }){
-    return(
-        <View>
-            <Button
-                title="Go to Index"
-                onPress={() =>
-                    navigation.navigate('Index')
-                }
-             />
-            <Text>HI scan</Text>
-        </View>
-    )
+export default function Scan({ navigation }) {
+  return (
+    <View>
+      <Button
+        title="Go to Index"
+        onPress={() =>
+          navigation.navigate('Index')
+        }
+      />
+      <Text>HI scan</Text>
+    </View>
+  )
 }
