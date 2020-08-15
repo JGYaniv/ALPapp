185138cf209600c01d6c124c566d489e8a1d56d5
zohaib <khaliqzohaib92@gmail.com>
2020-08-15 12:02:02 -0400

    Add email to migration

--- server/src/migrations/20200815154520-create-user.js
+++ server/src/migrations/20200815154520-create-user.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      email: {
+        type: Sequelize.STRING
+      },
       firstname: {
         type: Sequelize.STRING
       },
