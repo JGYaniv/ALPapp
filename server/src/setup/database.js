71526d59d0eae81e1f06b644119c55cf67e6e03f
zohaib <khaliqzohaib92@gmail.com>
2020-08-15 00:56:40 -0400

    Create database.json file to have seperate configs for dev and prod env's and also create database connection file

---
+++ server/src/setup/database.js
@@ -0,0 +1,25 @@
+import { Sequelize} from 'sequelize';
+
+import databaseConfig from '../config/database.json';
+
+const databaseConfigEnv = databaseConfig[process.env.NODE_ENV];
+
+// Create new database connection
+const connection  = new Sequelize(databaseConfigEnv.database, databaseConfigEnv.username, databaseConfigEnv.password, {
+    host: databaseConfigEnv.host,
+    dialect: databaseConfigEnv.dialect
+});
+
+// Auth database connection
+connection.
+authenticate()
+.then(() => {
+console.info('INFO - Database connected.')
+})
+.catch(err => {
+console.error('ERROR - Unable to connect to the database:', err)
+});
+
+
+export default connection;
+
