515825a2c0cfb6b052a61e6172bbf64e3385c6b9
Dhruva <tugme10@gmail.com>
2020-11-09 21:32:17 +0530

    Refactor: BookService

---
+++ client/lib/services/book_service.dart
@@ -0,0 +1,28 @@
+import 'package:ALPapp/models/book.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class BookService {
+  FirebaseFirestore _db;
+  CollectionReference _booksRef;
+
+  BookService() {
+    _db = FirebaseFirestore.instance;
+    _booksRef = _db.collection('books');
+  }
+
+  addBook(Book book) {
+    //Add new Book to the books collection
+    //TODO Security rules needed
+    _booksRef
+        .add({
+          'title': book.title,
+        })
+        .then((value) => print("${book.title} Added"))
+        .catchError((error) => print("Failed to add book: $error"));
+  }
+
+  Stream<QuerySnapshot> getBooks() {
+    //To be used with a StreamBuilder
+    return _booksRef.snapshots();
+  }
+}
