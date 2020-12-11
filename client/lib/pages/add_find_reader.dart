f89b7a499602e42b4885e747346b181d6f97e8e2
TUGTEN <tugzehn@gmail.com>
2020-12-11 13:34:04 +0530

    Resolved dependencies in pubspec

--- client/lib/pages/add_find_reader.dart
+++ client/lib/pages/add_find_reader.dart
@@ -1,6 +1,4 @@
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
 
 class AddFindReader extends StatefulWidget {
