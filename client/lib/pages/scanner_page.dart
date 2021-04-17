e0973dea76772f5e56fbe111a12b5772685ea8ad
Jonathan <yoniyaniv@gmail.com>
2021-04-17 13:10:50 -0400

    created placeholder pages for navbar

---
+++ client/lib/pages/scanner_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ScannerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A), child: Text("Scanner Page"));
+  }
+}
+
